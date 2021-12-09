05b247c6f538b001d5bd26b70e1d7b40fb32d50d
Raffay Syed <raffayhusayn@gmail.com>
2021-12-09 03:17:38 -0500

    Started a spring boot application using Spring Initializer and added only the spring boot web dependency

---
+++ src/main/java/com/fclass/bootapp/SpringBootBoilerPlateApplication.java
@@ -0,0 +1,13 @@
+package com.fclass.bootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBoilerPlateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBoilerPlateApplication.class, args);
+    }
+
+}
