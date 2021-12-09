8d4574b031b9ce13fd64ccbe558d9955cc034d1c
Raffay Syed <raffayhusayn@gmail.com>
2021-12-09 05:15:26 -0500

    Add Controller for home.jsp
    -- Add HomeController and request mapped it to home.jsp
    -- Add Tomcat Jasper dependency to maven to support JSP by spring boot

---
+++ src/main/java/com/fclass/bootapp/HomeController.java
@@ -0,0 +1,16 @@
+package com.fclass.bootapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//Name could be anything, it doesn't have to be Home to Control the home.jsp View
+@Controller
+public class HomeController {
+
+
+    @RequestMapping("home")
+    public String showHome(){
+        System.out.println("in the showHome method");
+        return "home.jsp";
+    }
+}
--- src/main/java/com/fclass/bootapp/SpringBootBoilerPlateApplication.java
+++ src/main/java/com/fclass/bootapp/SpringBootBoilerPlateApplication.java
@@ -2,12 +2,18 @@ package com.fclass.bootapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static org.springframework.boot.SpringApplication.*;
 
 @SpringBootApplication
 public class SpringBootBoilerPlateApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootBoilerPlateApplication.class, args);
+
+        ConfigurableApplicationContext context = run(SpringBootBoilerPlateApplication.class, args);
+        HomeController homeController = context.getBean(HomeController.class);
+        homeController.showHome();
     }
 
 }
