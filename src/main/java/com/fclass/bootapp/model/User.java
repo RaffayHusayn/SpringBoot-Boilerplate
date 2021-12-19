15ea1371091712c9b655c240a81181e0a302544a
Raffay Syed <raffayhusayn@gmail.com>
2021-12-18 23:21:40 -0500

    Add User Class and annotate it with @Entity to store username and passwords for the application also created UserRepository to allow CRUD operations

--- src/main/java/com/fclass/bootapp/model/User.java
+++ src/main/java/com/fclass/bootapp/model/User.java
@@ -1,2 +1,49 @@
-package com.fclass.bootapp.model;public class User {
+package com.fclass.bootapp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "user")
+public class User {
+    @Id
+    @Column(name = "Id")
+    private int id;
+    @Column(name="Username")
+    private String username;
+    @Column(name="Password")
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/fclass/bootapp/model/UserRepository.java
+++ src/main/java/com/fclass/bootapp/model/UserRepository.java
@@ -1,2 +1,7 @@
-package com.fclass.bootapp.model;public class UserRepository {
+package com.fclass.bootapp.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findUserByUsername(String username);
 }
